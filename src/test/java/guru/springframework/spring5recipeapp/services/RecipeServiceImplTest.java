9d13f04b15284ffeccf2816825c5ad10f263d15e
Ythalo Rossy <ythalorossy@gmail.com>
2019-01-23 00:28:00 -0500

    Added remote origin, initial commit and push

---
+++ src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,44 @@
+package guru.springframework.spring5recipeapp.services;
+
+import guru.springframework.spring5recipeapp.domain.Recipe;
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    private RecipeService recipeService;
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
